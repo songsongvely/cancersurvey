8fbd1ba175c18e17a2ea8b4010d8ed4a6f2b01ee
shushcorp <shush.co.kr@gmail.com>
2025-10-16 19:54:08 +0900

    feat: add expandable cancer risk education sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import ConsentPanel from '@/components/ConsentPanel'
+import HealthEducationPanel from '@/components/HealthEducationPanel'
 
 export default function HomePage() {
   return (
@@ -19,6 +20,7 @@ export default function HomePage() {
         </div>
       </section>
       <ConsentPanel />
+      <HealthEducationPanel />
     </div>
   )
 }
